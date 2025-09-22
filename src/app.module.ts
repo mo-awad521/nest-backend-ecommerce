8533d1fde162ed9b964e3a363d7e3923c1ff8e58
Mohammad Alawad <engmohammadalawad@gmail.com>
2025-09-22 22:37:03 +0300

    project structure

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+
+@Module({
+  controllers: [AppController],
+  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      database: 'nest_ecommerce',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+})
+@Module({
+  imports: [TypeOrmModule.forRoot()],
+})
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
